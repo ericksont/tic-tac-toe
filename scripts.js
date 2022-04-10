499a8df2cbf5a6b77484c2793c21751e6a55ec66
Erickson Tavares <erickson@ericksontavares.com.br>
2022-04-10 14:53:06 -0300

    check winner

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-let player = null;
+let player, winner = null;
 let selectedPlayer = document.getElementById("selected-player");
+let selectedWinner = document.getElementById("winner-player");
 let squares = document.getElementsByClassName("squere");
 
 function chooseSquare(id){
@@ -18,6 +19,7 @@ function chooseSquare(id){
         player = 'X';
 
     changePlayer(player);
+    checkWinner();
 }
 
 function changePlayer(value){
@@ -37,11 +39,69 @@ function checkWinner(){
     let square9 = document.getElementById(9);
 
 
+    if(checkSequence(square1, square2, square3)){
+        changeSquereColor(square1, square2, square3)
+        setWinner(square1)
+        return
+    }
+    if(checkSequence(square4, square5, square6)){
+        changeSquereColor(square4, square5, square6)
+        setWinner(square4)
+        return
+    }
+    if(checkSequence(square7, square8, square9)){
+        changeSquereColor(square7, square8, square9)
+        setWinner(square7)
+        return
+    }
+
+    if(checkSequence(square1, square4, square7)){
+        changeSquereColor(square1, square4, square7)
+        setWinner(square1)
+        return
+    }
+    if(checkSequence(square2, square5, square8)){
+        changeSquereColor(square2, square5, square8)
+        setWinner(square2)
+        return
+    }
+    if(checkSequence(square3, square6, square9)){
+        changeSquereColor(square3, square6, square9)
+        setWinner(square3)
+        return
+    }
+
+    if(checkSequence(square1, square5, square9)){
+        changeSquereColor(square1, square5, square9)
+        setWinner(square1)
+        return
+    }
+    if(checkSequence(square3, square5, square7)){
+        changeSquereColor(square3, square5, square7)
+        setWinner(square3)
+        return
+    }
 
 }
 
-function checkSequence() {
-    
+function checkSequence(square1, square2, square3) {
+    let isEqual = false;
+    if(square1.innerHTML !== '-' && square1.innerHTML === square2.innerHTML && square2.innerHTML === square3.innerHTML)
+        isEqual = true;
+
+    return isEqual;
+
+}
+
+function changeSquereColor(square1, square2, square3){
+    square1.style.background = "#0f0";
+    square2.style.background = "#0f0";
+    square3.style.background = "#0f0";
+}
+
+function setWinner(squere){
+    winner = squere.innerHTML;
+    selectedWinner.innerHTML = winner;
 }
 
 changePlayer('X');
