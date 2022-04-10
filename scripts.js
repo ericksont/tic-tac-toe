c3cd5de561fa83102518ac2175ec1118758b218e
Erickson Tavares <erickson@ericksontavares.com.br>
2022-04-10 13:42:07 -0300

    set first player

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 let player = null;
+let selectedPlayer = document.getElementById("selected-player")
 
 function chooseSquare(id){
     console.log("Choose Square "+id);
+    let square = document.getElementById(id);
+    square.innerHTML = player;
+    square.style.color = "#000";
 }
+
+function changePlayer(value){
+    player = value;
+    selectedPlayer.innerHTML = player;
+}
+
+changePlayer('X');
